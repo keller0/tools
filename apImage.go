58193e0a75c5633873c520a4faf160be2e549d78
caijiaqi <caijiaqi2017@gmail.com>
2018-05-09 13:09:04 +0800

    modify apimage

--- apImage.go
+++ apImage.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/fatih/color"
 	"io"
 	"net/http"
 	"os"
 	"strings"
 	"sync"
+	"strconv"
 )
 
 func download(baseurl, image string) error {
@@ -20,7 +22,7 @@ func download(baseurl, image string) error {
 	defer file.Close()
 
 	resp, err := http.Get(url)
-	fmt.Println(resp.ContentLength)
+	// fmt.Println(resp.ContentLength)
 	if err != nil {
 		return err
 	}
@@ -34,18 +36,35 @@ func download(baseurl, image string) error {
 }
 
 func main() {
-	s := flag.Int("s", 1, "start")
-	e := flag.Int("e", 2, "end")
+	s := flag.Int("s", -1, "start")
+	e := flag.Int("e", -1, "end")
 	var baseurl string
 	flag.StringVar(&baseurl, "b", "http://www.ibiblio.org/apollo/ScansForConversion/Comanche055/", "base url")
 	flag.Parse()
-	len := *e - *s
+	numbers := []int{}
+	if *s < 0 || *e < 0 {
+		fmt.Println(flag.Args())
+		for _, s := range flag.Args() {
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			numbers = append(numbers, n)
+		}
+		fmt.Println(numbers)
+	}else{
+		for i:=0; i<(*e-*s); i++ {
+			numbers = append(numbers, *s+i)
+		}
+	}
+	yellow := color.New(color.FgYellow).SprintFunc()
 	var wg sync.WaitGroup
-	for i := 0; i < len; i++ {
-		image := imgName(*s + i)
+	for _, num := range numbers {
+		image := imgName(num)
 		wg.Add(1)
 		go func(image string) {
-			fmt.Println(baseurl+image)
+			fmt.Printf("%s%s\n", baseurl, yellow(image))
 			if err := download(baseurl, image); err != nil {
 				panic(err)
 			}
@@ -60,7 +79,7 @@ func main() {
 func imgName(x int) string {
 	tmp := fmt.Sprintf("%d", x)
 	if len(tmp) < 4 {
-		tmp = strings.Repeat("0", 4-len(tmp)) + tmp + ".jpg"
+		tmp = strings.Repeat("0", 4-len(tmp)) + tmp
 	}
-	return tmp
+	return tmp + ".jpg"
 }
